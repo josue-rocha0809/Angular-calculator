78687f4c212debe630783e45eee78bdd56851de4
Josue Rocha <josue.rocha0809@gmail.com>
2021-04-14 01:28:22 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule
